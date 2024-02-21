24cee6d5452f5d165be32d674c7db981d029ef30
Arkadiusz Mazurek <arkadiuszmazurek13@gmail.com>
2024-02-21 01:06:50 +0100

    Added constants file and new mode for ball

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 import pygame
 from pygame.locals import *
 
+from constants import *
+
 
 class Snake:
     def __init__(self, x, y):
@@ -155,18 +157,50 @@ class Apple:
             self.y = SCREEN_HEIGHT - 50
             self.yVel *= -3
 
+    def flee(self):
+        vectorToTargetX = self.x - player.x
+        vectorToTargetY = self.y - player.y
+
+        vectorToTargetMagnitude = vectorToTargetX ** 2 + vectorToTargetY ** 2
+        vectorToTargetNormalizedX = vectorToTargetX / vectorToTargetMagnitude
+        vectorToTargetNormalizedY = vectorToTargetY / vectorToTargetMagnitude
+
+        self.xVel += vectorToTargetNormalizedX * 75
+        self.yVel += vectorToTargetNormalizedY * 75
+
+        self.yVel += 0.1
+
+        self.x += self.xVel
+        self.y += self.yVel
+
+        if self.x < 0:
+            self.x = 0
+            self.xVel *= -1
+
+        if self.x > SCREEN_WIDTH - 50:
+            self.x = SCREEN_WIDTH - 50
+            self.xVel *= -1
+
+        if self.y < 0:
+            self.y = 0
+            self.yVel *= -1
+
+        if self.y > SCREEN_HEIGHT - 50:
+            self.y = SCREEN_HEIGHT - 50
+            self.yVel *= -1
+
     def update(self):
         if self.state == 1:
             self.move_fixed()
         if self.state == 2:
             self.move_fixed_gravity()
+        if self.state == 3:
+            self.flee()
+
 
         self.rect = Rect(self.x, self.y, 50, 50)
     def draw(self):
         pygame.draw.ellipse(screen, (255, 0, 0), self.rect)
-
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
 
 pygame.init()
 
@@ -191,6 +225,7 @@ while running:
             running = False
             break
 
+    # ------------- CONTROLS --------------- #
     keys = pygame.key.get_pressed()
 
     if keys[K_w]:
@@ -202,6 +237,7 @@ while running:
     if keys[K_d]:
         player.xVel += 0.5
 
+    # ------------- GAME LOGIC --------------- #
     player.update()
 
     apple.update()
@@ -209,6 +245,7 @@ while running:
 
     if player.check_self_collision():
         running = False
+
     if player.rect.colliderect(apple.rect):
 
         player.make_snake_longer()
@@ -216,6 +253,7 @@ while running:
 
         points += 1
 
+    # ------------- GRAPHICS --------------- #
     screen.fill((0, 0, 0))
     player.draw()
     apple.draw()
@@ -223,7 +261,8 @@ while running:
     text = font.render(f'Score: {points}', True, (255, 255, 255))
     screen.blit(text, (10, 10))
 
-    clock.tick(60)
+    # ------------- GAME UPDATE --------------- #
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
